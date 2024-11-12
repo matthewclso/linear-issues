e40d85c7bdb5d9504fbe9737e6f4fc14541c9fdf
Matthew So <matthewclso@gmail.com>
2024-11-12 01:56:39 +0000

    Query issues all at once and then group, add prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,13 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import { LinearClient, User } from "@linear/sdk";
-import { Issue } from "@linear/sdk/dist/_generated_sdk";
 
 const LINEAR_AUTHENTICATION_PROVIDER_ID = "linear";
 const LINEAR_AUTHENTICATION_SCOPES = ["read"];
+
+interface Issue {
+  identifier: string;
+  title: string;
+}
 
 interface IssueCategory {
   status: string;
@@ -24,140 +28,136 @@ async function getClient(): Promise<LinearClient> {
   const session = await vscode.authentication.getSession(
     LINEAR_AUTHENTICATION_PROVIDER_ID,
     LINEAR_AUTHENTICATION_SCOPES,
-    { createIfNone: true }
+    { createIfNone: true },
   );
 
   if (!session) {
-    vscode.window.showErrorMessage(
-      `Linear login failed.`
-    );
+    vscode.window.showErrorMessage(`Linear login failed.`);
   }
 
   const linearClient = new LinearClient({
     accessToken: session.accessToken,
   });
-  
+
   return linearClient;
 }
 
-interface ProjectsStatesResponse {
-  data: {
-    team: {
-      projects: {
-        nodes: {
-          id: string;
-          name: string;
-        }[];
+function groupBy<T, K extends keyof T>(arr: T[], key: K): Record<string, T[]> {
+  return arr.reduce(
+    (result, item) => {
+      const groupKey = item[key];
+      (result[groupKey as string] ||= []).push(item);
+      return result;
+    },
+    {} as Record<string, T[]>,
+  );
+}
+
+interface IssuesResponse {
+  issues: {
+    nodes: {
+      identifier: string;
+      title: string;
+      team: {
+        name: string;
       };
-      states: {
-        nodes: {
-          id: string;
-          name: string;
-        }[];
+      project: {
+        name: string;
+      } | null;
+      state: {
+        name: string;
       };
+    }[];
+    pageInfo: {
+      hasNextPage: boolean;
+      endCursor: string;
     };
   };
 }
 
-async function getIssues(linearClient: LinearClient, me?: User): Promise<TeamCategory[]> {
-  const groups = [];
+async function getIssues(
+  linearClient: LinearClient,
+  me?: User,
+): Promise<TeamCategory[]> {
+  let issues = [];
 
-  const teams = await linearClient.teams().then((teams) => teams.nodes);
-
-  for (const team of teams) {
-    const data = await linearClient.client.rawRequest(`
-      query {
-        team(id: "${team.id}") {
-          projects {
-            nodes {
-              id
-              name
-            }
+  const query = `
+    query {
+      issues {
+        nodes {
+          identifier
+          title
+          team {
+            name
           }
-          states {
-            nodes {
-              id
-              name
-            }
+          project {
+            name
           }
+          state {
+            name
+          }
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
         }
       }
-    `) as ProjectsStatesResponse;
-    const projects = data.data.team.projects.nodes;
-    const states = data.data.team.states.nodes;
+    }
+  `;
+  let data = (await linearClient.client
+    .rawRequest(query)
+    .then((response) => response.data)) as IssuesResponse;
+  issues.push(...data.issues.nodes);
 
-    for (const project of projects) {
-      for (const state of states) {
-        const allIssues = [];
-        const filter = {
-          project: { id: { eq: project.id } },
-          state: { id: { eq: state.id } },
-        };
+  if (data.issues.pageInfo.hasNextPage) {
+    data = (await linearClient.client
+      .rawRequest(query, { after: data.issues.pageInfo.endCursor })
+      .then((response) => response.data)) as IssuesResponse;
+    issues.push(...data.issues.nodes);
+  }
 
-        if (me) {
-          let issues = await me.assignedIssues({ filter });
-          allIssues.push(...issues.nodes);
-          while (issues.pageInfo.hasNextPage) {
-            issues = await me.assignedIssues({ filter, after: issues.pageInfo.endCursor });
-            allIssues.push(...issues.nodes);
-          }
-        } else {
-          let issues = await linearClient.issues({ filter });
-          allIssues.push(...issues.nodes);
-          while (issues.pageInfo.hasNextPage) {
-            issues = await linearClient.issues({ filter, after: issues.pageInfo.endCursor });
-            allIssues.push(...issues.nodes);
-          }
-        }
+  issues = issues.map((issue) => ({
+    identifier: issue.identifier,
+    title: issue.title,
+    team: issue.team.name,
+    project: issue.project ? issue.project.name : "No project",
+    state: issue.state.name,
+  }));
 
-        if (allIssues.length > 0) {
-          groups.push({
-            team: team.name,
-            project: project.name,
-            state: state.name,
-            issues: allIssues,
-          });
-        }
-      }
+  // move 'No Project' issues to the beginning, starting from the end
+  let noProjectIndex = issues.length;
+  for (let i = issues.length - 1; i >= 0; i--) {
+    if (issues[i].project === "No project") {
+      const issue = issues.splice(i, 1)[0];
+      issues.unshift(issue);
+      noProjectIndex = i;
     }
   }
 
   const groupedIssues: TeamCategory[] = [];
-  for (const group of groups) {
-    const teamCategory = groupedIssues.find((teamCategory) => teamCategory.team === group.team);
-    if (teamCategory) {
-      const projectCategory = teamCategory.projects.find((projectCategory) => projectCategory.project === group.project);
-      if (projectCategory) {
-        const stateCategory = projectCategory.states.find((stateCategory) => stateCategory.status === group.state);
-        if (stateCategory) {
-          stateCategory.issues.push(...group.issues);
-        } else {
-          projectCategory.states.push({
-            status: group.state,
-            issues: group.issues,
-          });
-        }
-      } else {
-        teamCategory.projects.push({
-          project: group.project,
-          states: [{
-            status: group.state,
-            issues: group.issues,
-          }],
+
+  let teams = groupBy(issues, "team");
+  for (const team in teams) {
+    const projects = groupBy(teams[team], "project");
+    const teamCategory: TeamCategory = {
+      team,
+      projects: [],
+    };
+    for (const project in projects) {
+      const states = groupBy(projects[project], "state");
+      const projectCategory: ProjectCategory = {
+        project,
+        states: [],
+      };
+      for (const state in states) {
+        projectCategory.states.push({
+          status: state,
+          issues: states[state],
         });
       }
-    } else {
-      groupedIssues.push({
-        team: group.team,
-        projects: [{
-          project: group.project,
-          states: [{
-            status: group.state,
-            issues: group.issues,
-          }],
-        }],
-      });
+      teamCategory.projects.push(projectCategory);
     }
+    groupedIssues.push(teamCategory);
   }
 
   return groupedIssues;
@@ -165,29 +165,34 @@ async function getIssues(linearClient: LinearClient, me?: User): Promise<TeamCat
 
 export function activate(context: vscode.ExtensionContext) {
   const allIssuesProvider = new LinearIssueProvider();
-  vscode.window.registerTreeDataProvider('allIssues', allIssuesProvider);
-  vscode.commands.registerCommand('linear-issues.refreshAllIssues', async () => {
-    const linearClient = await getClient();
-    allIssuesProvider.refresh(await getIssues(linearClient));
-  });
+  vscode.window.registerTreeDataProvider("allIssues", allIssuesProvider);
+  vscode.commands.registerCommand(
+    "linear-issues.refreshAllIssues",
+    async () => {
+      const linearClient = await getClient();
+      allIssuesProvider.refresh(await getIssues(linearClient));
+    },
+  );
 
   const myIssuesProvider = new LinearIssueProvider();
-  vscode.window.registerTreeDataProvider('myIssues', myIssuesProvider);
-  vscode.commands.registerCommand('linear-issues.refreshMyIssues', async () => {
+  vscode.window.registerTreeDataProvider("myIssues", myIssuesProvider);
+  vscode.commands.registerCommand("linear-issues.refreshMyIssues", async () => {
     const linearClient = await getClient();
     const me = await linearClient.viewer;
     myIssuesProvider.refresh(await getIssues(linearClient, me));
   });
 
-  vscode.commands.executeCommand('linear-issues.refreshAllIssues');
-  vscode.commands.executeCommand('linear-issues.refreshMyIssues');
+  Promise.all([
+    vscode.commands.executeCommand("linear-issues.refreshAllIssues"),
+    vscode.commands.executeCommand("linear-issues.refreshMyIssues"),
+  ]);
 }
 
 class TeamCategoryNode extends vscode.TreeItem {
   constructor(
     public readonly label: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-    public readonly projects: ProjectCategory[]
+    public readonly projects: ProjectCategory[],
   ) {
     super(label, collapsibleState);
   }
@@ -197,7 +202,7 @@ class ProjectCategoryNode extends vscode.TreeItem {
   constructor(
     public readonly label: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-    public readonly states: IssueCategory[]
+    public readonly states: IssueCategory[],
   ) {
     super(label, collapsibleState);
   }
@@ -207,7 +212,7 @@ class IssueCategoryNode extends vscode.TreeItem {
   constructor(
     public readonly label: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-    public readonly issues: Issue[]
+    public readonly issues: Issue[],
   ) {
     super(label, collapsibleState);
   }
@@ -217,17 +222,24 @@ class IssueNode extends vscode.TreeItem {
   constructor(
     public readonly label: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-    public readonly issue: Issue
+    public readonly issue: Issue,
   ) {
     super(label, collapsibleState);
   }
 }
 
-type IssuesTreeNode = TeamCategoryNode | ProjectCategoryNode | IssueCategoryNode | IssueNode;
+type IssuesTreeNode =
+  | TeamCategoryNode
+  | ProjectCategoryNode
+  | IssueCategoryNode
+  | IssueNode;
 
 class LinearIssueProvider implements vscode.TreeDataProvider<IssuesTreeNode> {
-  private _onDidChangeTreeData: vscode.EventEmitter<IssuesTreeNode | undefined> = new vscode.EventEmitter<IssuesTreeNode | undefined>();
-  readonly onDidChangeTreeData: vscode.Event<IssuesTreeNode | undefined> = this._onDidChangeTreeData.event;
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    IssuesTreeNode | undefined
+  > = new vscode.EventEmitter<IssuesTreeNode | undefined>();
+  readonly onDidChangeTreeData: vscode.Event<IssuesTreeNode | undefined> =
+    this._onDidChangeTreeData.event;
 
   private categories: TeamCategory[];
 
@@ -242,22 +254,60 @@ class LinearIssueProvider implements vscode.TreeDataProvider<IssuesTreeNode> {
   }
 
   // Get the tree item for a specific issue
-  getTreeItem(element: IssuesTreeNode): vscode.TreeItem | Thenable<vscode.TreeItem> {
+  getTreeItem(
+    element: IssuesTreeNode,
+  ): vscode.TreeItem | Thenable<vscode.TreeItem> {
     return element;
   }
 
   // Get the children (issues) to be displayed in the tree
   getChildren(element?: IssuesTreeNode): Thenable<IssuesTreeNode[]> {
     if (!element) {
-      return Promise.resolve(this.categories.map((category) => new TeamCategoryNode(category.team, vscode.TreeItemCollapsibleState.Collapsed, category.projects)));
+      return Promise.resolve(
+        this.categories.map(
+          (category) =>
+            new TeamCategoryNode(
+              category.team,
+              vscode.TreeItemCollapsibleState.Collapsed,
+              category.projects,
+            ),
+        ),
+      );
     }
 
     if (element instanceof TeamCategoryNode) {
-      return Promise.resolve(element.projects.map((project) => new ProjectCategoryNode(project.project, vscode.TreeItemCollapsibleState.Collapsed, project.states)));
+      return Promise.resolve(
+        element.projects.map(
+          (project) =>
+            new ProjectCategoryNode(
+              project.project,
+              vscode.TreeItemCollapsibleState.Collapsed,
+              project.states,
+            ),
+        ),
+      );
     } else if (element instanceof ProjectCategoryNode) {
-      return Promise.resolve(element.states.map((state) => new IssueCategoryNode(state.status, vscode.TreeItemCollapsibleState.Collapsed, state.issues)));
+      return Promise.resolve(
+        element.states.map(
+          (state) =>
+            new IssueCategoryNode(
+              state.status,
+              vscode.TreeItemCollapsibleState.Collapsed,
+              state.issues,
+            ),
+        ),
+      );
     } else if (element instanceof IssueCategoryNode) {
-      return Promise.resolve(element.issues.map((issue) => new IssueNode(`${issue.identifier} ${issue.title}`, vscode.TreeItemCollapsibleState.None, issue)));
+      return Promise.resolve(
+        element.issues.map(
+          (issue) =>
+            new IssueNode(
+              `${issue.identifier} ${issue.title}`,
+              vscode.TreeItemCollapsibleState.None,
+              issue,
+            ),
+        ),
+      );
     }
 
     return Promise.resolve([]);
